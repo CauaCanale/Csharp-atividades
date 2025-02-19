3b18383a62b1a25df861284a12383c49633270ff
Caua <caua.cferreira2@gmail.com>
2025-02-19 11:56:38 -0300

    .

---
+++ DamajuCad/DamajuCad/CadClientes.cs
@@ -0,0 +1,68 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DamajuCad
+{
+    public partial class CadClientes : Form
+    {
+        public CadClientes()
+        {
+            InitializeComponent();
+        }
+        
+        private void Cadastrar_Click(object sender, EventArgs e)
+        {
+
+            String conexaoString = "Server=localhost; Port=3306; Database=bd_damaju; Uid=root; Pwd=;";
+
+            string query = "INSERT INTO tb_cliente ( ID_Clientes, Nome, Senha, Email, Cep, Cpf, Numero, Telefone ) VALUES (@Nome, @Email, @Cpf, @Telefone)";
+
+            using (MySqlConnection conexao = new MySqlConnection(conexaoString))
+            {
+                try
+                {
+                    conexao.Open();
+                    //Crie o comando SQL
+                    using (MySqlCommand comando = new MySqlCommand(query, conexao))
+                    {
+
+                        //Adicionar os parâmentros com os valores dos TextBox
+                        comando.Parameters.AddWithValue("@Nome", Nome.Text);
+                        comando.Parameters.AddWithValue("@Email", Email.Text);
+                        comando.Parameters.AddWithValue("@Cpf", Cpf.Text);
+                        comando.Parameters.AddWithValue("@Telefone", Telefone.Text);
+
+
+                        // Executa o comando de inserção
+                        comando.ExecuteNonQuery();
+
+                        Nome.Text = "";
+                        Email.Text = "";
+                        Cpf.Text = "";
+                        Telefone.Text = "";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // em caso de erro, exiba menssagem do erro
+                    MessageBox.Show("Erro: " + ex.Message);
+                }
+            }
+        }
+        
+        private void Cadastrar_Clientes_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}    
+
---
+++ DamajuCad/DamajuCad/Gerenciamento_Produtos.cs
@@ -0,0 +1,102 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DamajuCad
+{
+    public partial class Gerenciamento_Produtos : Form
+    {
+        public Gerenciamento_Produtos()
+        {
+            InitializeComponent();
+        }
+        private void RemoverProdutos_Click(object sender, EventArgs e)
+        {
+            if (Gerenciar_Produtos.SelectedRows.Count > 0)
+            {
+                //Pega ID do cliente da linha selecionada
+                int clienteID = Convert.ToInt32(Gerenciar_Produtos.SelectedRows[0].Cells["ID_Cliente"].Value);
+
+                DialogResult result = MessageBox.Show("Tem certeza que deseja excluir este cliente?", "Confirmar Exclusão", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    string connectionString = "Server=localhost; Port=3306; Database=bd_damaju; Uid=root; Pwd=;";
+
+                    try
+                    {
+                        using (MySqlConnection consulta = new MySqlConnection(connectionString))
+                        {
+                            consulta.Open();
+
+                            string listagem = "DELETE FROM tb_produtos WHERE ID_Produto = @ID_Produto";
+
+                            using (MySqlCommand cmd = new MySqlCommand(listagem, consulta))
+                            {
+                                cmd.Parameters.AddWithValue("ID_Produto", clienteID);
+
+                                int rowsAffected = cmd.ExecuteNonQuery();
+
+                                if (rowsAffected > 0)
+                                {
+                                    MessageBox.Show("Cliente excluido com sucesso!");
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Falha ao excluir o cliente");
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Erro:" + ex.Message);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, selecione um cliente para excluir");
+                }
+            }
+        }
+        private void PesquisarProdutos_Click(object sender, EventArgs e)
+        {
+            //Defina sua string de conexão com o banco
+            string connectionString = "Server=localhost; Port=3306; Database=bd_damaju; Uid=root; Pwd=;";
+
+            try
+            {
+                using (MySqlConnection consulta = new MySqlConnection(connectionString))
+                {
+                    consulta.Open();
+
+                    string listagem = "SELECT Id_produto, Nome, Valor, Categoria FROM tb_produtos";
+
+                    using (MySqlCommand cmd = new MySqlCommand(listagem, consulta))
+                    {
+                        MySqlDataReader reader = cmd.ExecuteReader();
+
+                        DataTable dadosClientes = new DataTable();
+                        dadosClientes.Load(reader);
+
+                        Gerenciar_Produtos.DataSource = dadosClientes;
+                    }
+                }
+            }
+
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao listar os Clientes:" + ex.Message);
+            }
+        }
+    }
+}
+
